783bee8c0d118577f3d6edb5feaf24077c91bbaa
Jan Francis Baldoza <you@example.com>
2022-11-03 19:22:24 -0400

    added random map generator function

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 var world = [
-    [1,1,1,1,1],
-    [1,0,2,2,1],
-    [1,2,1,2,1],
-    [1,2,2,2,1],
-    [1,2,2,2,1],
-    [1,2,1,2,1],
-    [1,2,2,2,1],
-    [1,2,2,2,1],
-    [1,2,1,2,1],
-    [1,2,2,2,1],
-    [1,1,1,1,1],
+    // [1,1,1,1,1],
+    // [1,0,2,2,1],
+    // [1,3,1,2,1],
+    // [1,2,2,2,1],
+    // [1,2,2,2,1],
+    // [1,2,1,2,1],
+    // [1,2,2,2,1],
+    // [1,2,2,2,1],
+    // [1,2,1,2,1],
+    // [1,2,2,2,1],
+    // [1,1,1,1,1],
 ];
 var worldDict = {
     0: 'blank',
     1: 'wall',
-    2: 'sushi'
+    2: 'sushi',
+    3: 'onigiri'
 }
 
-let points = document.getElementById('points')
-let score = 0
+var points = document.getElementById('points')
+var score = 0
+
+var worldheight = 15
+var worldwidth = 15
+
 
 function drawWorld() {
     output = "";
-    for(var row = 0; row < world.length; row++) {
+    for (var i=0; i<worldheight; i++){
+        world.push([i])
         output += "<div class = 'row'>"
-        for(var x = 0; x < world[row].length; x++) {
-            output += "<div class = '"+ worldDict[world[row][x]] +"'></div>"
+
+        for(var x=0; x<worldwidth; x++){
+            if(i == 0 || i == 14 || x == 0 || x == 14){
+                world[i].push(0)
+                output += "<div class = '"+ worldDict[1] +"'></div>"
+            } 
+            else {
+                world[i].push(Math.floor(Math.random() * 4));
+                output += "<div class = '"+ worldDict[world[i][x]] +"'></div>"
+            }
         }
         output += "</div>"
     }
@@ -66,11 +80,15 @@ document.onkeydown = function(e){
         }
     }
     if(world[ninjaman.y][ninjaman.x] == 2){ // kung yung ninjaman ay 2 
-        world[ninjaman.y][ninjaman.x] = 0;   // ninjaworld magiging 0
-        score++                             // pag world is 0 plus 1 sa score
+        world[ninjaman.y][ninjaman.x] = 0;  // ninjaworld magiging 0
+        score = score + 10                 // pag world is 0 plus 1 sa score
         points.innerText = score          
     }
-    // world[ninjaman.y][ninjaman.x] = 0;
+    if(world[ninjaman.y][ninjaman.x] == 3){ // kung yung ninjaman ay 2 
+        world[ninjaman.y][ninjaman.x] = 0;  // ninjaworld magiging 0
+        score = score + 5                 // pag world is 0 plus 1 sa score
+        points.innerText = score          
+    }
     drawNinjaman()
     drawWorld()
 }  
